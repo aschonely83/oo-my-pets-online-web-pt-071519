f3c6e5dc414fea8a44c751711d6809e0963d6e86
Adam Schonely <aschonely@gmail.com>
2019-08-18 13:40:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :cat, :dog 
+  attr_accessor :cats, :dog 
   attr_reader :name, :species
   
   @@all = []
@@ -27,6 +27,6 @@ class Owner
   end
   
   def buy_cat(cat)
-    @@all[:cat] << Cat.new(cat)
+    @@all[:cats] << Cat.new(cat)
   end  
 end
